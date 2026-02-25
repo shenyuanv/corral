bf77210909e8b0f0b5966532f87f4bd593dc6b1c
Shen <shen@Mac.lan>
2026-02-25 19:59:28 +0800

    Disable browser cache on static files

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ const server = http.createServer((req, res) => {
 
   fs.readFile(filePath, (err, data) => {
     if (err) { res.writeHead(404); res.end('Not found'); return; }
-    res.writeHead(200, { 'Content-Type': types[ext] || 'text/plain' });
+    res.writeHead(200, { 'Content-Type': types[ext] || 'text/plain', 'Cache-Control': 'no-cache, no-store' });
     res.end(data);
   });
 });
